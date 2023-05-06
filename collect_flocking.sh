e5ce0f617892466e7353a5041fc2085dcb7456cf
Dulhan Jayalath <dhjayalath@protonmail.com>
2023-05-06 14:59:33 +0100

    Change iterations to 100

--- collect_flocking.sh
+++ collect_flocking.sh
@@ -4,14 +4,14 @@
 # Mandatory runs
 for i in {1..3}
 do
-  python policy.py --wandb_name flocking_sae_frozen --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_file weights/sae_flocking.pt --training_iterations 300 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
-  python policy.py --wandb_name flocking_noenc --seed $i -c flocking --model joippo --no_stand --training_iterations 300 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
-  python policy.py --wandb_name flocking_sae_policy --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_loss policy --training_iterations 300 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
+  python policy.py --wandb_name flocking_sae_frozen --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_file weights/sae_flocking.pt --training_iterations 100 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
+  python policy.py --wandb_name flocking_noenc --seed $i -c flocking --model joippo --no_stand --training_iterations 100 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
+  python policy.py --wandb_name flocking_sae_policy --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_loss policy --training_iterations 100 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
 done
 
 # Less important runs
 for i in {1..3}
 do
-  python policy.py --wandb_name flocking_sae_tune_recon --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_file weights/sae_flocking.pt --encoder_loss recon --training_iterations 300 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
-  python policy.py --wandb_name flocking_sae_tune_policy --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_file weights/sae_flocking.pt --encoder_loss policy --training_iterations 300 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
+  python policy.py --wandb_name flocking_sae_tune_recon --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_file weights/sae_flocking.pt --encoder_loss recon --training_iterations 100 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
+  python policy.py --wandb_name flocking_sae_tune_policy --seed $i -c flocking --model joippo --encoder sae --encoding_dim 32 --encoder_file weights/sae_flocking.pt --encoder_loss policy --training_iterations 100 --train_batch_size 2500 --sgd_minibatch_size 1024 --num_envs 4
 done
