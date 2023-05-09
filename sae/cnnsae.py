33e68776433948d26a45966bd797780c4c2678c3
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2023-05-09 11:13:25 +0100

    Add OG coins weights

--- sae/cnnsae.py
+++ sae/cnnsae.py
@@ -1,44 +1,7 @@
 from torch import nn
 from sae.model import AutoEncoder as SAE
-
-class ConvCAESAE(nn.Module):
-    def __init__(self, dim=768, hidden_dim=100):
-        super().__init__()
-        self.cnn = CNNAE(latent_dim=hidden_dim)
-        self.sae = SAE(dim=hidden_dim, hidden_dim=hidden_dim)
-
-    def forward(self, x, batch):
-        # x has shape (steps * envs * agents, ...)
-
-        # Encoder with ConvVAE
-        mu, logvar = self.cnn.encode(x)  # (steps * agents, 11, 11, 3)
-        z = self.cnn.reparameterize(mu, logvar)
-
-        # Encode + Decode with SAE
-        zr, _ = self.sae(z, batch=batch)  # (steps * agents, dim) ??
-
-        # Decode SAE reconstruction with ConvVAE
-        x_recon = self.cnn.decode(zr)  # (steps * agents, 88, 88, 3)
-
-        # Decode original latent with ConvVAE
-        x_cnn_recon = self.cnn.decode(z)
-
-        return x_recon, x_cnn_recon, mu, logvar
-
-    def encode(self, x, batch):
-        # Encode with Conv VAE
-        mu, logvar = self.cnn.encode(x)  # (steps * agents, 11, 11, 3)
-        z = self.cnn.reparameterize(mu, logvar)
-
-        # Encode with SAE
-        z_sae = self.sae.encoder(z, batch=batch)
-
-        return z_sae
-
-    def decode(self, z_sae):
-        z = self.sae.decoder(z_sae)
-        x = self.cnn.decode(z)
-        return x
+import torch
+from torch.nn import functional as F
 
 class ConvVAESAE(nn.Module):
     def __init__(self, dim=768, hidden_dim=100, n_agents=2, obs_w=88):
@@ -78,70 +41,6 @@ class ConvVAESAE(nn.Module):
         z = self.sae.decoder(z_sae)
         x = self.cnn.decode(z)
         return x
-
-class CNNAE(nn.Module):
-    def __init__(self,
-                 hidden_channels: int = 32,
-                 latent_dim: int = 20,
-                 act_fn: object = nn.LeakyReLU):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, hidden_channels, kernel_size=3, padding=1, stride=2),  # 32x32 => 16x16, 88x88 => 44x44
-            act_fn(),
-            nn.Conv2d(hidden_channels, hidden_channels, kernel_size=3, padding=1),
-            act_fn(),
-            nn.Conv2d(hidden_channels, 2 * hidden_channels, kernel_size=3, padding=1, stride=2),
-            # 16x16 => 8x8, 44x44 => 22x22
-            act_fn(),
-            nn.Conv2d(2 * hidden_channels, 2 * hidden_channels, kernel_size=3, padding=1),
-            act_fn(),
-            nn.Conv2d(2 * hidden_channels, 2 * hidden_channels, kernel_size=3, padding=1, stride=2),
-            # 8x8 => 4x4, 22x22 => 11x11
-            act_fn(),
-            nn.Flatten(),  # Image grid to single feature vector
-            # nn.Linear(hidden_channels * 2 * 4 * 4, latent_dim)
-            nn.Linear(hidden_channels * 2 * 11 * 11, latent_dim)
-        )
-
-        self.decoder = nn.Sequential(
-            # Linear
-            # nn.Linear(latent_dim, hidden_channels * 2 * 4 * 4),
-            nn.Linear(latent_dim, hidden_channels * 2 * 11 * 11),
-            act_fn(),
-            # Shape
-            # nn.Unflatten(1, (2 * hidden_channels, 4, 4)),
-            nn.Unflatten(1, (2 * hidden_channels, 11, 11)),
-            # CNN
-            nn.ConvTranspose2d(2 * hidden_channels, 2 * hidden_channels, kernel_size=3, output_padding=1, padding=1,
-                               stride=2),  # 4x4 => 8x8
-            act_fn(),
-            nn.Conv2d(2 * hidden_channels, 2 * hidden_channels, kernel_size=3, padding=1),
-            act_fn(),
-            nn.ConvTranspose2d(2 * hidden_channels, hidden_channels, kernel_size=3, output_padding=1, padding=1,
-                               stride=2),  # 8x8 => 16x16
-            act_fn(),
-            nn.Conv2d(hidden_channels, hidden_channels, kernel_size=3, padding=1),
-            act_fn(),
-            nn.ConvTranspose2d(hidden_channels, 3, kernel_size=3, output_padding=1, padding=1, stride=2),
-            # 16x16 => 32x32
-            nn.Tanh()  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
-        )
-
-    def encode(self, x):
-        return self.encoder(x)
-
-    def decode(self, z):
-        return self.decoder(z)
-
-    def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return encoded, decoded
-
-
-import torch
-from torch.nn import functional as F
-
 
 class VAE(nn.Module):
     def __init__(self,
